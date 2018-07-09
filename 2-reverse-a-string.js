33824ecb589ae536d012edc4971c7cacdb89eade
Linh Huynh <35814458+linh4@users.noreply.github.com>
2018-07-09 19:53:50 -0400

    Update 2-reverse-a-string.js

--- 2-reverse-a-string.js
+++ 2-reverse-a-string.js
@@ -1,14 +1,14 @@
 /* Reverse the provided string. Your result must be a string. */
 
 function reverseString(str) {
-    let result = "";
+    let result = '';
     for (let i = str.length - 1; i >= 0; i--) {
       result += str[i];
     }
     return result;
 }
 
-reverseString("hello");
+reverseString('hello');
 
 
 /*
