f48396e183d2d7077e27c2c152bd0f35240ed0cb
Linh <linh44h@gmail.com>
2018-07-09 23:01:50 -0400

    Add Code

---
+++ 8-truncate-a-string.js
@@ -0,0 +1,25 @@
+/* Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+Return the truncated string with a ... ending. */
+
+function truncateString(str, num) {
+    if (str.length > num) {
+      return str.slice(0, num) + '...';
+    } else {
+      return str;
+    }
+}
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+/*
+function truncateString(str, num) {
+    let result = '';
+    if (str.length > num){
+      for (let i = 0; i < num; i++){
+          result += str[i];
+        }
+      return result + '...';
+    } else {
+      return str;
+    }
+}
+*/
